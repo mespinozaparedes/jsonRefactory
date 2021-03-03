5db8b76980134b95492b759edaa5103fdca12429
Miguel Espinoza <espinozaparedes.miguel@gmail.com>
2021-03-02 21:05:17 -0500

    commit inicial

---
+++ src/main/java/pe/com/entelgy/jsonrefactory/JsonRefactoryApplication.java
@@ -0,0 +1,13 @@
+package pe.com.entelgy.jsonrefactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonRefactoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonRefactoryApplication.class, args);
+	}
+
+}
